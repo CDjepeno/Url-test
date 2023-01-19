1cf90191e3dcebf836e835239a52e21f611c778f
CDjepeno <djepeno.christopher@gmail.com>
2023-01-19 15:02:52 +0100

    :art: add docs with swagger

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import * as swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../../swagger.json'
 import urlRoutes from './routes/urlRoutes';
 
 const app = express()
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(urlRoutes)
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); 
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`server started, listening PORT ${PORT}`))
